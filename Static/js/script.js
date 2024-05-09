708da8fbd34c65f4240cfc17794c44326dbd5876
Vero1357 <veroyopo2010@gmail.com>
2024-05-09 20:07:24 -0300

    vero

---
+++ Static/js/script.js
@@ -0,0 +1,88 @@
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
+
+const expresiones = {
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	nacim: /^.{4,12}$/, // 4 a 12 digitos.
+	email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+}
+
+const campos = {
+	nombre: false,
+	apellido: false,
+	email: false,
+	nacim: false,
+	imagen: false,
+	consul: false
+}
+
+const validarFormulario = (e) => {
+	switch (e.target.name) {
+		case "nombre":
+			validarCampo(expresiones.nombre, e.target, 'nombre');
+			break;
+		case "apellido":
+			validarCampo(expresiones.apellido, e.target, 'apellido');
+			break;
+		case "email":
+			validarCampo(expresiones.email, e.target, 'email');
+			break;
+		case "nacim":
+			validarCampo(expresiones.nacim, e.target, 'nacim');
+			break;
+		case "imagen":
+			validarCampo(expresiones.imagen, e.target, 'imagen');
+			break;
+		case "consul":
+			validarCampo(expresiones.consul, e.target, 'consul');
+			break;
+		
+	}
+
+}
+
+const validarCampo = (expresion, input, campo) => {
+	if (expresion.test(input.value)) {
+		document.getElementById('grupo__${campo}').classList.remove('form-control-incorrecto');
+		document.getElementById('grupo__${campo}').classList.add('form-control-correcto');
+		document.querySelector('#grupo__${campo} i').classList.add('fa-check-circle');
+		document.querySelector('#grupo__${campo} i').classList.remove('fa fa-times-circle');
+		document.querySelector('#grupo__${campo} .form-input-error').classList.remove('form-input-error-activo');
+		campos[campo] = true;
+	} else {
+		document.getElementById('grupo__${campo}').classList.add('form-control-incorrecto');
+		document.getElementById('grupo__${campo}').classList.remove('form-control-correcto');
+		document.querySelector('#grupo__${campo} i').classList.add('fa fa-times-circle');
+		document.querySelector('#grupo__${campo} i').classList.remove('fa-check-circle');
+		document.querySelector('#grupo__${campo} .form-input-error').classList.add('form-input-error-activo');
+		campos[campo] = false;
+	}
+}
+
+inputs.forEach((input) => {
+	input.addEventListener('keyup', validarFormulario);
+	input.addEventListener('blur', validarFormulario);
+});
+
+formulario.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+
+	const terminos = document.getElementById('terminos');
+	if (campos.nombre && campos.apellido && campos.email && campos.nacim && campos.imagen && campos.consul && terminos.cheked) {
+		formulario.reset();
+
+		document.getElementById('form-mensaje-exito').classList.add('form-mensaje-exito-activo');
+		setTimeout(() => {
+			document.getElementById('form-mensaje-exito').classList.remove('form-mensaje-exito-activo');
+		}, 5000);
+
+		document.querySelectorAll('form-control-correcto').forEach((icono) => {
+			icono.classList.remove('form-control-correcto');
+		});
+	} else {
+		document.getElementById('form-mensaje').classList.add('form-mensaje-activo');
+	}
+});
+
